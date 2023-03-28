fa0e6ccfcb87d7eec2028e6208d86ea461214951
EscapeFromJava <reske.artur@gmail.com>
2023-03-28 22:08:39 +0300

    feature add cat

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Base");
         System.out.println("Base Commit 1");
+
+        System.out.println("Feature Commit 1");
+        System.out.println("Feature Commit 2");
+        System.out.println("Feature Commit 3");
+
+        Person person = Person.builder()
+                .name("Max")
+                .age(33)
+                .build();
+
+        Cat cat = Cat.builder()
+                .name("Barsik")
+                .color(Color.WHITE)
+                .owner(person)
+                .build();
+
+        System.out.println(cat);
     }
 }
