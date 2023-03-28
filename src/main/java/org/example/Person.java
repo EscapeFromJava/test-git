76b831fa95e6655e75be93cfc81e5cfd312693a0
EscapeFromJava <reske.artur@gmail.com>
2023-03-28 21:51:49 +0300

    feature add person

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Person {
+    private String name;
+    private Integer age;
+}
