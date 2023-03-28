67b2aca514f6c5d5e37f37c0f3a5328d1a073f5a
EscapeFromJava <reske.artur@gmail.com>
2023-03-28 21:46:34 +0300

    feature commit 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Base");
         System.out.println("Base Commit 1");
+
+        System.out.println("Feature Commit 1");
     }
 }
